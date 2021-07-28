2932d8303d33b814801abfbecc0c1f1431bff9ac
fuadtag <fuadjt@code.edu.az>
2021-07-28 21:41:13 +0400

    Init

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Title -->
+    <title>Multipurpose Template</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="{{asset('img/core-img/favicon.ico')}}">
+
+    <!-- Core Stylesheet -->
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+
+    <!-- Responsive Stylesheet -->
+    <link rel="stylesheet" href="{{asset('css/responsive.css')}}">
+
+</head>
+
+@yield('content')
+
+</html>
